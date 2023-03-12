fda05972ee0bd51a625ea2cb2890f853841486e2
cruciaal <vitaliimarchenko@rocketmail.com>
2023-03-12 12:59:11 +0200

    12

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 for (let i = 1; i <= 5; i++) {
-  console.log('\n=>' + i + '<=\n');
+  console.log('\n=>' + i + '<=\n\n');
   for (let j = 1; j <= 5; j++) {
     console.log(i + ' x ' + j + ' = ' + i * j);
   }
